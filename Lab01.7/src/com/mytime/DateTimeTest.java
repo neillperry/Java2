03028e11083f36e6b2cb18ee27075a0f91be3a95
neillperry <chainsaw.bucktooth@gmail.com>
2020-04-28 09:23:38 -0700

    finished part 1 of 1.7

--- Lab01.7/src/com/mytime/DateTimeTest.java
+++ Lab01.7/src/com/mytime/DateTimeTest.java
@@ -8,20 +8,26 @@
  */
 package com.mytime;
 
+import java.awt.image.SinglePixelPackedSampleModel;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.Date;
 import java.time.LocalDateTime;
+import java.text.DateFormat;
 
 public class DateTimeTest {
   /**
    * main() calls each of the test methods in turn (currently commented out).
    * To run one test method at a time, uncomment the call to the one you want to execute.
    */
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ParseException {
     testNow();
-    // testCreate();
-    // testParse();
-    // testFormat();
+    testCreate();
+    testParse();
+    testFormat();
   }
 
   /**
@@ -40,21 +46,27 @@ public class DateTimeTest {
   /**
    * TASK: implement the TODOs and print your results.
    */
-  public void testCreate() {
+  public static void testCreate() {
     // TODO: create your birthday via of(). What day of the week were you born on?
-        
+    LocalDate dob = LocalDate.of(1776, 7, 4 );
+
     // TODO: use of() to create a value representing the 1st lunar landing - it happened on 7/20/69 at 3:18pm Eastern Time.
-    // Note: ignore time-zone, just assume Eastern Time is the local time.
+    LocalDate moon = LocalDate.of(1969, 7, 20 );
+    System.out.println(dob);
+    System.out.println(moon);
   }
 
   /**
    * TASK: implement the TODOs and print your results.
    */
-  public static void testParse() {
+  public static void testParse() throws ParseException {
     // TODO: create your birthday by parsing a text representation in standard format.
-    
+    LocalDate dob = LocalDate.parse("1776-07-04");
     // OPTIONAL: now create it by parsing text in the form "2/6/2014" (this is Feb 6, not Jun 2).
-    //LocalDate dob = LocalDate.parse(args[1]);
+    String birthdate = "07/04/1776";
+    LocalDate dob2 = LocalDate.parse(birthdate, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+    System.out.println(dob);
+    System.out.println(dob2);
   }
   
   /**
@@ -64,9 +76,15 @@ public class DateTimeTest {
     LocalDate hastings = LocalDate.of(1066, 10, 14);
     
     // TODO: 10/14/1066
-    
+    String formattedDate2 = hastings.format(DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+    System.out.println(formattedDate2);
+
     // TODO: 14-10-1066
-    
+    String formattedDate3 = hastings.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    System.out.println(formattedDate3);
+
     // OPTIONAL: October 14, 1066
+    String formattedDate = hastings.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+    System.out.println(formattedDate);
   }
 }
