2bdcf20625bb54e11698fc28fcf116e3fbd4d569
neillperry <chainsaw.bucktooth@gmail.com>
2020-04-29 15:48:42 -0700

    added skeleton tests for 4.1E

--- Lab04.1-Extra/test/com/javatunes/catalog/InMemoryCatalogTest.java
+++ Lab04.1-Extra/test/com/javatunes/catalog/InMemoryCatalogTest.java
@@ -1,11 +1,74 @@
 package com.javatunes.catalog;
 
-
+import static org.junit.Assert.*;
+import java.util.Collection;
+import java.util.Map;
+import java.util.HashSet;
+import org.junit.Test;
+import java.sql.Date;
+import java.time.LocalDate;
 
 
 public class InMemoryCatalogTest {
 
 
-    
+    @Test
+    public void testFindByName() {
+    }
 
+    @Test
+    public void testFindByKeyword() {
+    }
+
+    @Test
+    public void testFindByCategory() {
+    }
+
+    @Test
+    public void testGetAll() {
+    }
+
+    @Test
+    public void testSameArtistSameTitle() {
+    }
+
+    @Test
+    public void testRockItemsByPrice() {
+    }
+
+    @Test
+    public void testHowManySold() {
+    }
+
+    @Test
+    public void testAveragePriceOf() {
+    }
+
+    @Test
+    public void testCheapestInGenre() {
+    }
+
+    @Test
+    public void testAveragePriceInGenre() {
+    }
+
+    @Test
+    public void testAllLowerThan() {
+    }
+
+    @Test
+    public void testSellAnyofType() {
+    }
+
+    @Test
+    public void testSortPopTitles() {
+    }
+
+    @Test
+    public void testEightiesMusicAtPrice() {
+    }
+
+    @Test
+    public void testGenres() {
+    }
 }
